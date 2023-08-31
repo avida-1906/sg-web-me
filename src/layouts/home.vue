451a5507a369cb71af5f089a09bf9376c30342e5
less1973 <jama7488@gmail.com>
2023-08-31 19:32:49 +0800

    定义全局css变量

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,8 +1,60 @@
 <template>
-  <main>
-    <RouterView />
-    <div>
-      [Home Layout]
+  <main class="wrap">
+    <div class="left-sidebar">
+      123
+    </div>
+    <div class="main-content">
+      <header class="navigation">
+        <div class="group">
+          <div class="container">
+            Logo
+          </div>
+        </div>
+      </header>
+      <div class="scrollable">
+        <RouterView />
+      </div>
     </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+.wrap {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.left-sidebar {
+  width: 60px;
+  background-color: var(--app-secondary-dark);
+}
+.main-content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  flex-direction: column;
+
+  & > .navigation {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 60px;
+    background-color: red;
+
+    & > .group {
+      width: 100%;
+      padding: 0 3vw;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
+
+  & > .scrollable {
+    flex: 1;
+    overflow: auto;
+  }
+}
+</style>
