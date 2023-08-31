bde9b6b3bce9f9f0ba15631818bea0df28b83b2b
wade <wade@munuyk.com>
2023-08-31 16:14:34 +0800

    feat(icon):图标列表

--- src/pages/components/index.vue
+++ src/pages/components/index.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup>
+import ids from 'virtual:svg-icons-names'
+
+function copyIcon(id: string) {
+  return `<AppIcon :name="${id}" style="font-size: 50px;" />`
+}
 </script>
 
 <template>
   <section class="box">
     <h3>AppIcon</h3>
-    <AppIcon name="chat" style="font-size: 100px;" />
+    <ul class="icon-wrap">
+      <li v-for="id in ids" :key="id" class="item" @click="() => copyIcon(id)">
+        <AppIcon :name="id.slice(5)" style="font-size: 50px;" />
+      </li>
+    </ul>
   </section>
   <section class="box" />
 </template>
@@ -13,5 +22,17 @@
 .box {
     padding: 20px;
     border: 4px solid red;
+}
+
+.icon-wrap {
+  display: flex;
+  list-style: none;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  .item {
+    cursor: pointer;
+    width: 60px;
+    // height: 120px;
+  }
 }
 </style>
