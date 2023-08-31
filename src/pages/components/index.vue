35775281e048459f9daf3b9c5dd6f7b651e9d6b5
wade <wade@munuyk.com>
2023-08-31 16:56:23 +0800

    feat(icon):icon name

--- src/pages/components/index.vue
+++ src/pages/components/index.vue
@@ -2,26 +2,26 @@
 import ids from 'virtual:svg-icons-names'
 
 function copyIcon(id: string) {
-  return `<AppIcon :name="${id}" style="font-size: 50px;" />`
+  return `<AppIcon name="${id}" style="font-size: 50px;" />`
 }
 </script>
 
 <template>
   <section class="box">
-    <h3>AppIcon</h3>
-    <ul class="icon-wrap">
+    <h3>图标 AppIcon</h3>
+    <ul class="icon-wrap m0 p0">
       <li v-for="id in ids" :key="id" class="item" @click="() => copyIcon(id)">
         <AppIcon :name="id.slice(5)" style="font-size: 50px;" />
       </li>
     </ul>
   </section>
-  <section class="box" />
 </template>
 
 <style lang="scss" scoped>
 .box {
     padding: 20px;
-    border: 4px solid red;
+    position: relative;
+    text-align: center;
 }
 
 .icon-wrap {
@@ -32,7 +32,6 @@ function copyIcon(id: string) {
   .item {
     cursor: pointer;
     width: 60px;
-    // height: 120px;
   }
 }
 </style>
