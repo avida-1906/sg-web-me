cc33c7af7ebeac58e2cce47922b6879e5db18b94
Roger <Roger@123.com>
2023-09-04 13:45:41 +0800

    feat✨: demo路由

---
+++ src/pages/demo.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const activeTab = ref('Button')
+const tabList = [
+  'Button',
+]
+</script>
+
+<template>
+  <div class="list">
+    <BaseButton v-for="l in tabList" :key="l" @click="activeTab = l">
+      {{ l }}
+    </BaseButton>
+  </div>
+  <div v-if="activeTab === 'Button'">
+    <BaseButton @click="myCopy('hello world')">
+      Click
+    </BaseButton>
+    <BaseButton bg-style="primary">
+      Click
+    </BaseButton>
+    <BaseButton bg-style="secondary">
+      Click
+    </BaseButton>
+    <BaseButton type="text">
+      Click
+    </BaseButton>
+    <BaseButton type="line">
+      Click
+    </BaseButton>
+    <BaseButton type="round-line-left">
+      Click
+    </BaseButton>
+    <BaseButton type="round-line-right">
+      Click
+    </BaseButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
