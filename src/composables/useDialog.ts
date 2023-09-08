f073db804879e0948420f4b2e1f52e782ea8448c
wade <wade@munuyk.com>
2023-09-08 10:59:55 +0800

    feat(dialog):remove node

--- src/composables/useDialog.ts
+++ src/composables/useDialog.ts
@@ -3,30 +3,32 @@ import BaseDialog from '~/components/BaseDialog.vue'
 export function useDialog({ title, icon, default: defaultSlot }: { title: string; icon: string; default: () => Component }) {
   const show = ref(false)
   const app = ref()
+  const div = ref()
 
   const openDialog = () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-
+    div.value = document.createElement('div')
+    document.body.appendChild(div.value)
     app.value = createApp(h(BaseDialog, {
       title,
       icon,
       show: true,
       onClose: () => {
         app.value.unmount()
-        div.remove()
+        div.value.remove()
         closeDialog()
       },
     }, {
       default: () => defaultSlot(),
     }))
-    app.value.mount(div)
+    app.value.mount(div.value)
     show.value = true
   }
 
   function closeDialog() {
     if (app.value)
       app.value.unmount()
+    if (div.value)
+      div.value.remove()
 
     show.value = false
   }
