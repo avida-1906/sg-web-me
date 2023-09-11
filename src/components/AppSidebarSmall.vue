55a0f89eee553780e316986a3d4f5428d62aaf3a
wade <wade@munuyk.com>
2023-09-11 15:32:43 +0800

    feat(优化):youhua

---
+++ src/components/AppSidebarSmall.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup name="app-sidebar-small">
+
+</script>
+
+<template>
+  <section class="tg-app-sidebar-small">
+    111
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.tg-app-sidebar-small {
+
+}
+</style>
