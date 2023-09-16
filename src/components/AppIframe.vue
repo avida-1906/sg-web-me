afad4cc19902dc38a5f694bc78a2d2c56243b3e8
Roger <Roger@123.com>
2023-09-16 19:28:41 +0800

    feat✨: 1

--- src/components/AppIframe.vue
+++ src/components/AppIframe.vue
@@ -1,5 +1,10 @@
 <script setup lang='ts'>
 const gameUrl = ref('https://static-live.hacksawgaming.com/1263/1.11.3/index.html?language=zh&channel=desktop&gameid=1263&mode=1&token=ce6f762e-a59b-417e-83aa-9f72d335a35f&lobbyurl=https%3A%2F%2Fstake.com&currency=CAD&partner=stake&env=https://rgs-cu.hacksawgaming.com/api')
+
+const isRealMoneyMode = ref(false)
+function onSwitch(v: boolean) {
+  console.log('切换真钱模式', v)
+}
 </script>
 
 <template>
@@ -13,15 +18,56 @@ const gameUrl = ref('https://static-live.hacksawgaming.com/1263/1.11.3/index.htm
 
           <div class="footer">
             <div class="left">
+              <!-- 全屏 -->
               <div class="button">
                 <BaseIcon name="uni-full-screen" />
               </div>
+              <!-- 影院模式 -->
+              <VMenu placement="top">
+                <div class="button">
+                  <BaseIcon name="uni-theatre" />
+                </div>
+                <template #popper>
+                  <div class="tiny-menu-item-title">
+                    <!-- 禁用剧院模式 -->
+                    启用剧院模式
+                  </div>
+                </template>
+              </VMenu>
+
+              <!-- 实时统计 -->
+              <VMenu placement="top">
+                <div class="button">
+                  <BaseIcon name="uni-trend" />
+                </div>
+                <template #popper>
+                  <div class="tiny-menu-item-title">
+                    <!-- 关闭实时统计 -->
+                    打开实时统计
+                  </div>
+                </template>
+              </VMenu>
+
+              <!-- 收藏游戏 -->
+              <VMenu placement="top">
+                <div class="button">
+                  <BaseIcon name="chess-star" />
+                </div>
+                <template #popper>
+                  <div class="tiny-menu-item-title">
+                    <!-- 取消收藏 -->
+                    收藏游戏
+                  </div>
+                </template>
+              </VMenu>
             </div>
             <div class="logo">
-              1
+              <BaseIcon name="app-logo" />
             </div>
             <div class="right">
-              1
+              <span>试玩模式</span>
+              <BaseSwitch v-model="isRealMoneyMode" class="switch" @change="onSwitch" />
+              <span>真钱模式</span>
             </div>
           </div>
         </div>
@@ -51,6 +97,7 @@ const gameUrl = ref('https://static-live.hacksawgaming.com/1263/1.11.3/index.htm
 
   .iframe-wrapper {
     position: relative;
+
     &::before {
       content: "";
       display: block;
@@ -69,20 +116,45 @@ const gameUrl = ref('https://static-live.hacksawgaming.com/1263/1.11.3/index.htm
       border: none;
     }
   }
-  .footer{
+
+  .footer {
     height: 63px;
     padding: var(--tg-spacing-8);
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: 100%;
-    background:var(--tg-secondary-dark);
+    background: var(--tg-secondary-dark);
     color: var(--tg-secondary-light);
-    .left{
-      font-size:16px;
-      .button{
+
+    .left {
+      font-size: var(--tg-font-size-default);
+      display: flex;
+      padding-right: var(--tg-spacing-8);
+      border-right: 1px solid var(--tg-secondary-main);
+
+      .button {
         padding: var(--tg-spacing-button-padding-vertical-sm) var(--tg-spacing-button-padding-horizontal-sm);
         cursor: pointer;
+
+        &:hover {
+          --tg-icon-color: var(--tg-text-white);
+        }
+      }
+    }
+
+    .logo {
+      font-size: 68px;
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+      font-size: var(--tg-font-size-default);
+      padding-right: var(--tg-spacing-24);
+
+      .switch {
+        margin: 0 var(--tg-spacing-8);
       }
     }
   }
