9c7bc79e0bdd7b215c52d79835039f63e806a6a2
JANGO16888 <145426576+JANGO16888@users.noreply.github.com>
2023-09-20 11:06:46 +0800

    feat✨: 引用 共用 进度条 组件

--- src/components/AppPercentage.vue
+++ src/components/AppPercentage.vue
@@ -8,10 +8,15 @@ const props = withDefaults(defineProps<Props>(), {
   total: 0,
   percentage: 0,
 })
+
+const componentPercent = computed(() => {
+  return Number(((props.percentage / props.total) * 100).toFixed(2))
+})
 </script>
 
 <template>
   <div class="app-percentage">
+    <BaseProgress class="app-percentage-bar" :width="180" :percent="componentPercent" :stroke-width="10" :show-info="false" />
     <span class="text">正显示 {{ props.total }} 款游戏中的 {{ props.percentage }} 个</span>
   </div>
 </template>
@@ -19,6 +24,9 @@ const props = withDefaults(defineProps<Props>(), {
 <style lang='scss' scoped>
 .app-percentage {
   text-align: center;
+  &-bar {
+    margin: 0 auto;
+  }
   .text {
     color: var(--tg-text-lightgrey);
     font-size: var(--tg-font-size-default);
