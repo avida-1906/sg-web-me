0ca13a1574cf6d8775c1d0b43a73ba8e8052881d
wade <wade@munuyk.com>
2023-09-23 17:22:16 +0800

    feat(menu):优化

---
+++ src/components/AppAccordionMenu.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import type { MenuItem } from '~/composables/useApiMenuData'
+
+interface Props {
+  menuItem: MenuItem
+}
+
+withDefaults(defineProps<Props>(), {})
+
+function handleClickHead() {
+  // const t = setTimeout(() => {
+  //   innerRef.value.scrollTo({ top: document.getElementById('sports-hot-game-football')!.offsetTop - 60, behavior: 'smooth' })
+  //   clearTimeout(t)
+  // }, 500)
+}
+
+const { menuItemClick } = useApiMenuData()
+
+function handleClickItem(item: any) {
+  menuItemClick(item)
+}
+</script>
+
+<template>
+  <section>
+    <BaseAccordion
+      v-if="menuItem.list && menuItem.list.length" :dom-id="menuItem.domId" :menu-info="menuItem" :auto-show="menuItem.expand"
+      @click-head="handleClickHead"
+      @click-item="handleClickItem"
+    >
+      <template #default="{ menuItem: info }">
+        <AppMenuItem :menu-item="info" />
+      </template>
+    </BaseAccordion>
+    <AppMenuItem v-else :menu-item="menuItem" @click="() => handleClickItem(menuItem)" />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMenuItem.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import type { MenuItem } from '~/composables/useApiMenuData'
+
+interface Props {
+  menuItem: MenuItem
+}
+
+withDefaults(defineProps<Props>(), {})
+</script>
+
+<template>
+  <div class="tg-app-menu-item">
+    <div class="menu-item">
+      <BaseIcon :name="menuItem.icon" />
+      <span>{{ menuItem.title }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tg-app-menu-item {
+  .menu-item {
+    cursor: pointer;
+    display: flex;
+    white-space: nowrap;
+    align-items: center;
+    gap: var(--tg-spacing-8);
+    height: 45px;
+    padding: var(--tg-spacing-13) var(--tg-spacing-16);
+    border-radius: var(--tg-radius-default);
+    font-size: var(--tg-font-size-default);
+    font-weight: var(--tg-font-weight-semibold);
+    color: var(--tg-text-white);
+    transition: background-color 0.2s;
+    width: 100%;
+    transform: none!important;
+    background-color: transparent;
+    overflow: hidden;
+    &:hover, &.active {
+      background-color: var(--tg-secondary-main);
+    }
+  }
+}
+</style>
