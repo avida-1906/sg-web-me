50f446b3080625a52af18d8cc60b2ba167c16ba8
wade <wade@munuyk.com>
2023-09-27 14:42:35 +0800

    doc:注释

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -10,22 +10,9 @@ const router = createRouter({
   routes,
   scrollBehavior(to, from, savedPosition) {
     setTimeout(() => {
-      // document.getElementById('main-content-scrollable')?.scrollTo(0, 0)
       document.querySelector('.only-for-get-width')?.scrollIntoView({ behavior: 'auto', block: 'nearest', inline: 'center' })
     }, 0)
   },
-})
-
-router.beforeEach((to, from, next) => {
-  next()
-})
-
-router.beforeResolve(async (to) => {
-  if (to.meta.layoutLoading) {
-    setTimeout(() => {
-      console.log('layout loading finish')
-    }, 300)
-  }
 })
 
 export function install(app: App<Element>) {
