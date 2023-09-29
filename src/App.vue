247e2528374bfe14ab559ae481fc0b9af2f7cec6
less1973 <jama7488@gmail.com>
2023-09-29 18:45:57 +0800

    添加渲染中间态

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
+const { bool: isShow, setFalse: setShowFalse, setTrue: setShowTrue } = useBoolean(false)
+let timer: NodeJS.Timeout | null = null
+
+function init() {
+  timer = setTimeout(() => {
+    console.error('Loading timeout')
+    setShowTrue()
+  }, 250)
+}
+
+function clearTimer() {
+  if (timer) {
+    clearTimeout(timer)
+    timer = null
+  }
+}
+
+function handleUpdated() {
+  clearTimer()
+  setShowFalse()
+}
+
+init()
 </script>
 
 <template>
-  <RouterView />
+  <RouterView @vue:updated="handleUpdated" />
+  <div v-if="isShow" class="loading center">
+    <BaseLoading />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.loading {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-color: var(--tg-primary-main);
+  inset: 0;
+  z-index: var(--tg-z-index-top);
+}
+</style>
