f2492000511c21382079670bc5ab02f73b0dde03
wade <wade@munuyk.com>
2023-10-02 16:01:05 +0800

    feat(chat):聊天室订单、@用户

---
+++ src/components/AppChatMsgOrder.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+interface Props {
+  type: 'casino' | 'sport'
+  order: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <BaseButton type="text">
+    <span class="chat-msg-order">
+      <span class="label">{{ type }}: #</span>
+      <span class="order">{{ order }}</span>
+    </span>
+  </BaseButton>
+</template>
+
+<style lang="scss" scoped>
+button {
+  padding: 0 !important;
+  height: auto;
+  font-size: var(--tg-font-size-default);
+  color: var(--tg-secondary-light);
+  line-height: inherit;
+}
+.chat-msg-order {
+  .label {
+    text-transform: capitalize;
+  }
+}
+</style>
