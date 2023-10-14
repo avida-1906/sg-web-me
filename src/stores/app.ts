065ed00770314daae189cae0e20a886235ae92e2
less1973 <jama7488@gmail.com>
2023-10-14 19:18:17 +0800

    协议为null，返回空数组

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -30,6 +30,7 @@ export function generateCurrencyData(
     const balanceWithSymbol = symbol + balance
     const cur = currencyConfig[i].cur
     const bank_tree = currencyConfig[i].bank_tree
+    const contract_type = currencyConfig[i].contract_type ?? []
     list.push({
       type,
       balance,
@@ -37,6 +38,7 @@ export function generateCurrencyData(
       balanceWithSymbol,
       cur,
       bank_tree,
+      contract_type,
     })
   }
   return list
