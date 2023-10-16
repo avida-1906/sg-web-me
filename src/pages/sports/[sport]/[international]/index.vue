56bef1e66f76ccd3553bfd89baf29f3728f6d4ef
wade <wade@munuyk.com>
2023-10-16 11:18:57 +0800

    feat:index

---
+++ src/pages/sports/[sport]/[international]/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="tg-sports-country-index">
+    全部国家国际全部某种赛事
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tg-sports-country-index {
+
+}
+</style>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
