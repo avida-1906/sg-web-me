d5d52edef4c79a4b1f7ea1f4c2fd290c9f0aa52b
less1973 <jama7488@gmail.com>
2023-10-20 16:34:33 +0800

    添加落地页

---
+++ src/pages/download.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="center download">
+    <a href="">
+      <BaseButton>
+        下载Android Apk
+      </BaseButton>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.download {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
