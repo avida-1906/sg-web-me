02f593f2b9201d3cfcee6657695be71c66b2cb39
less1973 <jama7488@gmail.com>
2023-11-01 20:00:05 +0800

    Update genImg.sh

--- scripts/genImg.sh
+++ scripts/genImg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set +e
 
+# brew install joedrago/repo/avifenc
+# brew install webp
 
 
 # 生成 webp，avif 格式图片
@@ -27,10 +29,6 @@ function png2webp() {
     if [ -d $1"/"$file ]; then
       png2webp $1"/"$file
     else
-      # echo $1"/"$file
-      # echo ${1/$pngPath/$webpPath}
-      # echo ${file/.png/.webp}
-      # 判断webp下是否存在该文件夹，不存在则创建
       if [ ! -d ${1/$pngPath/$webpPath} ]; then
         mkdir -p ${1/$pngPath/$webpPath}
       fi
@@ -39,6 +37,18 @@ function png2webp() {
   done
 }
 
-function 
+function png2avif() {
+  for file in `ls $1`
+  do
+    if [ -d $1"/"$file ]; then
+      png2avif $1"/"$file
+    else
+      if [ ! -d ${1/$pngPath/$avifPath} ]; then
+        mkdir -p ${1/$pngPath/$avifPath}
+      fi
+      avifenc $1"/"$file ${1/$pngPath/$avifPath}"/"${file/.png/.avif}
+    fi
+  done
+}
 
 png2webp $pngPath
