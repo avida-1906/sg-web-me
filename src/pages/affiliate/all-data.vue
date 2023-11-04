083b45e487a243f9dc676080d00aaa7940c03046
wade <wade@munuyk.com>
2023-11-04 15:44:53 +0800

    feat:demo

--- src/pages/affiliate/all-data.vue
+++ src/pages/affiliate/all-data.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+const date = ref([])
 const currentPage = ref(1)
 </script>
 
 <template>
   <div>
+    <BaseDatePicker v-model="date" />
     <BasePagination v-model:current-page="currentPage" :total="1008" />
   </div>
 </template>
