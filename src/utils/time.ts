a691d5f64fec092ab6a59e9fd8230a3076dab1de
Roger <Roger@123.com>
2023-11-14 16:38:01 +0800

    feat✨: time

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -5,21 +5,22 @@ import 'dayjs/locale/zh-cn'
 import 'dayjs/locale/en'
 import 'dayjs/locale/vi'
 
-// 是否润年插件
 dayjs.extend(isLeapYear)
 dayjs.extend(weekday)
 
 const format = {
   'zh-cn': 'MM月DD日 ddd HH:mm',
   'en': 'ddd, MMM D h:mm A',
-  'vi': 'ddd, D MMMM h:mm A',
+  'vi': 'ddd, D MMMM h:mm',
 }
 
+/** 检查是否闰年 */
 export function timeCheckIsLeapYear(ts: number) {
   return dayjs(ts).isLeapYear()
 }
 
+/** 赛事时间转换 */
 export function timeFullTimeFormat(ts: number) {
   dayjs.locale('vi')
-  return dayjs(ts).format(format.vi)
+  return dayjs(ts).format(format.vi).replace('T', 'Th ').replace('tháng', 'Thg')
 }
