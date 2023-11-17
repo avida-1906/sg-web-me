2ebca50a93efa6f0e7b2b9364a282dffb63fb3e2
Roger <Roger@123.com>
2023-11-17 18:17:39 +0800

    feat✨: 体育收藏列表

--- src/components/AppSportsPageFavourites.vue
+++ src/components/AppSportsPageFavourites.vue
@@ -1,87 +1,54 @@
 <script lang="ts" setup>
-defineProps<{ onPage?: boolean }>()
+const { sportsFavoriteData } = storeToRefs(useSportsStore())
 
-const { sportLiveNavs, currentLiveNav } = storeToRefs(useSportsStore())
+const currentSi = ref(-1)
+/** 收藏数据根据球种组合 */
+const sportsFavoriteList = computed(() => {
+  if (sportsFavoriteData.value)
+    return sportsDataGroupBySport(sportsFavoriteData.value.list)
+
+  return []
+})
+const navs = computed(() => {
+  return sportsFavoriteList.value.map((a) => {
+    return {
+      si: a.si,
+      sn: a.sn,
+      count: a.list.length,
+      icon: 'spt-soccer',
+    }
+  })
+})
+const list = computed(() => {
+  if (sportsFavoriteData.value)
+    return sportsDataGroupByLeague(sportsFavoriteData.value.list.filter(a => a.si === currentSi.value))
+
+  return null
+})
+
+watch(sportsFavoriteList, (a) => {
+  if (a.length > 0)
+    currentSi.value = a[0].si
+})
 </script>
 
 <template>
-  <div class="tg-sports-favourites" :class="{ 'on-page': onPage }">
-    <AppSportsTab v-model="currentLiveNav" :list="sportLiveNavs" />
-    <div v-if="!onPage" class="layout-spacing">
-      <AppBetData mode="sports" />
+  <div class="tg-sports-favourites">
+    <AppSportsTab v-show="navs.length > 0" v-model="currentSi" :list="navs" />
+    <div v-if="list" class="market-wrapper">
+      <AppSportsMarket
+        v-for="item in list" :key="item.ci"
+        :is-standard="true"
+        :league-name="item.cn"
+        :event-count="item.list.length"
+        :event-list="item.list"
+        base-type="winner"
+      />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.tg-sports-favourites {
-  &.on-page {
-    margin-top: 0;
-  }
-}
-
-.dropdown {
-  margin-bottom: var(--tg-spacing-24);
-  display: inline-block;
-}
-
-.btn-arrow-down {
-  font-size: var(--tg-font-size-default);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: var(--tg-spacing-8);
-  line-height: 1.3;
-
-  .icon {
-    width: 18px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    .app-svg-icon {
-      font-size: var(--tg-font-size-xs);
-    }
-  }
-
-}
-
-.clear-all {
-  width: 100%;
-  padding:
-    var(--tg-spacing-button-padding-horizontal-xs) var(--tg-spacing-button-padding-vertical-xs);
-  font-weight: var(--tg-font-weight-semibold);
-  text-align: center;
-  font-size: var(--tg-font-size-default);
-  color: var(--tg-popper-color-bright);
-  cursor: pointer;
-  background-color: var(--tg-popper-bg-deep);
-}
-
-.base-check-pop-inner {
-  padding: var(--tg-spacing-button-padding-horizontal-xs);
-  display: flex;
-  flex-direction: column;
-  max-height: 340px;
-  &::-webkit-scrollbar-thumb {
-      background: var(--tg-secondary-light);
-    }
-}
-
-.check-item-label {
-  display: inline-flex;
-  align-items: center;
-  font-weight: var(--tg-font-weight-semibold);
-  font-size: var(--tg-font-size-default);
-  color: var(--tg-popper-color-default);
-  transition: all 0.2s;
-  letter-spacing: 0;
-  padding-top: 6px;
-  padding-bottom: 4px;
-  justify-content: space-between;
-  gap: 8px;
-}
-
 .market-wrapper {
   display: flex;
   flex-direction: column;
