f8a99ea1fa6def5652d620e626e955c827888bba
Roger <Roger@123.com>
2023-11-21 16:59:52 +0800

    perf👌: 我的投注loading、无数据缺省

--- src/components/AppSportsPageMyBet.vue
+++ src/components/AppSportsPageMyBet.vue
@@ -15,7 +15,7 @@ const typeOptions = [
   { label: t('sports_settled'), value: 1 },
 ]
 
-const { data } = useRequest(() => ApiSportBetList({
+const { data, loading } = useRequest(() => ApiSportBetList({
   kind: 'normal',
   settle: currentType.value,
   cur: currencyConfig[currentGlobalCurrency.value].cur,
@@ -53,7 +53,16 @@ watch(props, (a) => {
         />
       </div>
     </div>
-    <div class="slip-wrapper" :style="`column-count:${columnCount}`">
+    <div v-show="loading" class="loading">
+      <BaseLoading />
+    </div>
+    <div v-if="!loading && list.length === 0" class="empty">
+      <BaseEmpty icon="empty-2" :description="t('data_empty')" />
+    </div>
+    <div
+      v-if="!loading && list.length > 0"
+      class="slip-wrapper" :style="`column-count:${columnCount}`"
+    >
       <div v-for="item in list" :key="item.ono" class="child">
         <AppSportsMyBetSlip :data="item" />
       </div>
@@ -64,6 +73,13 @@ watch(props, (a) => {
 <style lang='scss' scoped>
 .title{
   margin-bottom: var(--tg-spacing-24);
+}
+.loading{
+  width: 100%;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .slip-wrapper {
   width: 100%;
