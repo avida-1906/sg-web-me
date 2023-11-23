19cfa554f8d02d5fa2219ff13e666cbfeb53326c
wade <wade@munuyk.com>
2023-11-23 14:03:17 +0800

    feat:lang

--- src/composables/useDialogThirdAuthForm.ts
+++ src/composables/useDialogThirdAuthForm.ts
@@ -1,11 +1,13 @@
 import AppAuthThirdFormDialog from '~/components/AppAuthThirdFormDialog.vue'
 
 export function useDialogThirdAuthForm() {
+  const { t } = useI18n()
+
   const {
     openDialog: openThirdAuthFormDialog,
     closeDialog: closeThirdAuthFormDialog,
   } = useDialog({
-    title: '注册 Auth',
+    title: `${t('reg')} Auth`,
     default: ({ data, ty }) => h(AppAuthThirdFormDialog, { data, ty }),
   })
 
