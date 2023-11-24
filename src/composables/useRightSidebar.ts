6b7c6e462e5ff3ceabd2f6b5cf0fa9de72cabc21
wade <wade@munuyk.com>
2023-11-24 19:33:34 +0800

    feat:lock

--- src/composables/useRightSidebar.ts
+++ src/composables/useRightSidebar.ts
@@ -26,6 +26,8 @@ export const useRightSidebar = createGlobalState(() => {
   const { bool: rightIsExpand, setTrue: setRightIsExpandTrue, setFalse: setRightIsExpandFalse } = useBoolean(defaultRightStatus)
   const { bool: rightContainerIs0, setTrue: setRightContainerIs0True, setFalse: setRightContainerIs0False } = useBoolean(!defaultRightStatus)
 
+  useLockScroll(rightIsExpand)
+
   function openRightSidebar(rightSidebarContent?: EnumRightSidebarContent) {
     // 打开右侧侧边栏
     setRightIsExpandTrue()
