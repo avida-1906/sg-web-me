7db9aa38e6e949cfcbf9dc6cb3005c20b774c05a
john <john@vgtech.org>
2023-11-24 19:43:07 +0800

    钱包卡死处理

--- src/components/AppWithdraw.vue
+++ src/components/AppWithdraw.vue
@@ -127,90 +127,92 @@ watch(() => props.currentNetwork, () => {
 </script>
 
 <template>
-  <!-- 虚拟币提款 -->
-  <div v-if="addrOptions.length" class="app-withdraw">
-    <BaseLabel
-      :label="`${activeCurrency?.type}${t('address')}`"
-      :current-currency="activeCurrency?.type"
-      must
-    >
-      <BaseSelect
-        v-model="address"
-        :options="addrOptions"
-        :msg="addressMsg"
-        small theme popper border
-        style="--tg-base-select-popper-style-padding-y: var(--tg-spacing-12)"
-        @focus="addressMsg && resetAddress()"
+  <div>
+    <!-- 虚拟币提款 -->
+    <div v-if="addrOptions.length" class="app-withdraw">
+      <BaseLabel
+        :label="`${activeCurrency?.type}${t('address')}`"
+        :current-currency="activeCurrency?.type"
+        must
       >
-        <template #label>
-          <span class="popper-label">
-            <AppCurrencyIcon
-              v-if="defaultAddress"
-              :currency-type="activeCurrency?.type"
-            />
-            {{ defaultAddress }}
-          </span>
-        </template>
-        <template #option="{ data: { item, parentWidth } }">
-          <div
-            class="scroll-x bank-options"
-            :style="{ width: `${parentWidth + 24}px` }"
-          >
-            <div class="option-row">
-              <AppCurrencyIcon :currency-type="activeCurrency?.type" />
-              <div class="bank-info">
-                <p>{{ item.label }}</p>
+        <BaseSelect
+          v-model="address"
+          :options="addrOptions"
+          :msg="addressMsg"
+          small theme popper border
+          style="--tg-base-select-popper-style-padding-y: var(--tg-spacing-12)"
+          @focus="addressMsg && resetAddress()"
+        >
+          <template #label>
+            <span class="popper-label">
+              <AppCurrencyIcon
+                v-if="defaultAddress"
+                :currency-type="activeCurrency?.type"
+              />
+              {{ defaultAddress }}
+            </span>
+          </template>
+          <template #option="{ data: { item, parentWidth } }">
+            <div
+              class="scroll-x bank-options"
+              :style="{ width: `${parentWidth + 24}px` }"
+            >
+              <div class="option-row">
+                <AppCurrencyIcon :currency-type="activeCurrency?.type" />
+                <div class="bank-info">
+                  <p>{{ item.label }}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </template>
-      </BaseSelect>
-    </BaseLabel>
-    <div class="amount">
-      <div class="top">
-        <span class="label">{{ t('amount') }}
-          <span style="color: var(--tg-text-error);">*</span></span>
-        <!-- <span class="us">US$0.00</span> -->
+          </template>
+        </BaseSelect>
+      </BaseLabel>
+      <div class="amount">
+        <div class="top">
+          <span class="label">{{ t('amount') }}
+            <span style="color: var(--tg-text-error);">*</span></span>
+          <!-- <span class="us">US$0.00</span> -->
+        </div>
+        <BaseInput
+          v-model="amount"
+          type="number"
+          :msg="amountMsg"
+          @blur="onAmountInput"
+          @on-right-button="maxNumber"
+        >
+          <!-- <template #right-icon>
+              <AppCurrencyIcon :currency-type="activeCurrency?.type" />
+            </template> -->
+          <template #right-button>
+            <span>{{ t('max') }}</span>
+          </template>
+        </BaseInput>
       </div>
-      <BaseInput
-        v-model="amount"
-        type="number"
-        :msg="amountMsg"
-        @blur="onAmountInput"
-        @on-right-button="maxNumber"
-      >
-        <!-- <template #right-icon>
-            <AppCurrencyIcon :currency-type="activeCurrency?.type" />
-          </template> -->
-        <template #right-button>
-          <span>{{ t('max') }}</span>
-        </template>
-      </BaseInput>
+      <BaseLabel :label="t('menu_title_settings_update_safepwd')" must>
+        <BaseInput v-model="paypwd" :msg="paypwdMsg" type="password" max="6" />
+      </BaseLabel>
+      <BaseButton bg-style="primary" size="md" @click="handleWithdraw">
+        {{ t('menu_title_settings_withdrawals') }}
+      </BaseButton>
+      <!-- <div class="tips">
+          <span>{{ t('withdrawal_min_amount') }} 0.00020000</span>
+          <AppCurrencyIcon class="currency-icon" :currency-type="activeCurrency?.type" />
+          <span>。{{ t('withdrawal_fee_tip') }}0.00007000</span>
+          <AppCurrencyIcon class="currency-icon" :currency-type="activeCurrency?.type" />
+          <span>{{ t('withdrawal_as_fee') }}。</span>
+        </div> -->
     </div>
-    <BaseLabel :label="t('menu_title_settings_update_safepwd')" must>
-      <BaseInput v-model="paypwd" :msg="paypwdMsg" type="password" max="6" />
-    </BaseLabel>
-    <BaseButton bg-style="primary" size="md" @click="handleWithdraw">
-      {{ t('menu_title_settings_withdrawals') }}
-    </BaseButton>
-    <!-- <div class="tips">
-        <span>{{ t('withdrawal_min_amount') }} 0.00020000</span>
-        <AppCurrencyIcon class="currency-icon" :currency-type="activeCurrency?.type" />
-        <span>。{{ t('withdrawal_fee_tip') }}0.00007000</span>
-        <AppCurrencyIcon class="currency-icon" :currency-type="activeCurrency?.type" />
-        <span>{{ t('withdrawal_as_fee') }}。</span>
-      </div> -->
+    <!-- 虚拟币地址添加 -->
+    <AppVirAddressDialog
+      v-else
+      is-withdraw
+      :currency-id="activeCurrency.cur"
+      :currency-name="activeCurrency.type"
+      :contract-id="getContractId"
+      style="--tg-app-vir-address-style-padding: 0"
+      @added="updateContract"
+    />
   </div>
-  <!-- 虚拟币地址添加 -->
-  <AppVirAddressDialog
-    v-else
-    is-withdraw
-    :currency-id="activeCurrency.cur"
-    :currency-name="activeCurrency.type"
-    :contract-id="getContractId"
-    style="--tg-app-vir-address-style-padding: 0"
-    @added="updateContract"
-  />
 </template>
 
 <style lang='scss' scoped>
