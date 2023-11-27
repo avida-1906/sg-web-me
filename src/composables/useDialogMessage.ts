61e979892545e7556d1f3e61c5319b1629811bf9
john <john@vgtech.org>
2023-11-27 15:04:45 +0800

    站内信

--- src/composables/useDialogMessage.ts
+++ src/composables/useDialogMessage.ts
@@ -1,10 +1,10 @@
 import AppDialogMessage from '~/components/AppDialogMessage.vue'
 
-export function useDialogMessage() {
+export function useDialogMessage(title: string) {
   const { t } = useI18n()
 
   const { openDialog: openMessageDialog, closeDialog: closeMessageDialog } = useDialog({
-    title: t('marquee'),
+    title,
     icon: 'navbar-notice',
     default: data => h(AppDialogMessage, { data }),
   })
