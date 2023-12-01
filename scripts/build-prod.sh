ca2f4154d1026d037be1560e306bda5f2d04d668
less1973 <jama7488@gmail.com>
2023-12-01 14:35:26 +0800

    添加build-fat shell 脚本

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -5,23 +5,19 @@ env=$1
 
 # 仓库地址
 repoUrl="http://munuyk.com/sg-release/web-sg.git"
-# 桌面下载目录
+# 桌面git仓库目录
 localRepoDir="$HOME/Desktop/work/build-file/web-sg"
-# 当前目录
-# currentDir=$(pwd)
 
-# if [ -n "$(git status -s)" ]
-# then
-#     echo "本地有未提交的代码，请先提交代码"
-#     exit 1
-# fi
+if [ -n "$(git status -s)" ]
+then
+    echo "本地有未提交的代码，请先提交代码"
+    exit 1
+fi
 
 pnpm build:$env
 
-# 进入 localRepoDir
 cd $localRepoDir
 
-# 拉取最新代码
 echo "开始拉取最新代码..."
 git pull origin main --no-rebase
 
@@ -31,7 +27,8 @@ cd -
 echo "开始复制文件..."
 cp -r ./dist/* $localRepoDir
 
-# 提交代码
+cd $localRepoDir
+
 echo "开始提交代码..."
 git add .
 
