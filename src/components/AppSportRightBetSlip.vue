5e8ebef6d66d781f5935326f0a75fcd06e3bed44
wade <wade@munuyk.com>
2023-12-11 11:36:32 +0800

    feat:a

--- src/components/AppSportRightBetSlip.vue
+++ src/components/AppSportRightBetSlip.vue
@@ -358,6 +358,7 @@ function betSuccessTip(widSuccessList: string[]) {
     : 1
   openNotify({
     type: 'wallet',
+    title: isBetSingle.value ? '单项投注' : '体育复式投注',
     message: () => h(
       AppSportBetSuccessNotify,
       {
