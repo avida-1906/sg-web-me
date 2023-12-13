36283c6d00f4b13daf0aa49aa80db8e4c3a520e7
john <john@vgtech.org>
2023-12-13 19:36:00 +0800

    padding

--- src/components/AppLoginRegisterBtns.vue
+++ src/components/AppLoginRegisterBtns.vue
@@ -18,13 +18,22 @@ function handleRegister() {
 
 <template>
   <div class="center btn-box">
-    <BaseButton type="text" custom-padding @click.stop="handleLogin">
+    <BaseButton
+      type="text" custom-padding
+      style="--tg-base-button-padding-y: var(--tg-spacing-button-padding-vertical-md);
+        --tg-base-button-padding-x: var(--tg-spacing-button-padding-horizontal-md);
+        --tg-base-button-text-default-color: var(--tg-text-white);"
+      @click.stop="handleLogin"
+    >
       {{ t('login') }}
     </BaseButton>
     <BaseButton
       custom-padding
       bg-style="primary"
-      style="--tg-base-button-font-size: 15px"
+      style="--tg-base-button-font-size: calc(var(--tg-font-size-default) + 1px);
+      --tg-base-button-padding-y: calc(var(--tg-spacing-button-padding-vertical-md) - 0.5px);
+      --tg-base-button-padding-x:
+      calc(var(--tg-spacing-button-padding-horizontal-md) - 1px);"
       @click.stop="handleRegister"
     >
       {{ t('reg') }}
@@ -33,9 +42,4 @@ function handleRegister() {
 </template>
 
 <style lang="scss" scoped>
-.btn-box{
-  --tg-base-button-padding-y: var(--tg-spacing-button-padding-vertical-md);
-  --tg-base-button-padding-x: var(--tg-spacing-button-padding-horizontal-md);
-  --tg-base-button-text-default-color: var(--tg-text-white);
-}
 </style>
