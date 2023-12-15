331ee2a6fe3bd8b565405c55266424e8f3a3ad62
wade <wade@munuyk.com>
2023-12-15 10:55:53 +0800

    feat:menu

--- src/pages/vip/rebate.vue
+++ src/pages/vip/rebate.vue
@@ -9,14 +9,6 @@ const { AllLanguages, userLanguage } = storeToRefs(useLanguageStore())
 const { list: platformList } = useList(ApiMemberPlatformList, {
   manual: false,
 })
-// const platformList = computed(() => {
-//   const arr: ProviderItem[] = []
-//   for (let i = 0; i < list.value.length; i++) {
-//     if (!arr.find(a => a.venue_id === list.value[i].venue_id))
-//       arr.push(list.value[i])
-//   }
-//   return arr
-// })
 
 const tab = ref('')
 
