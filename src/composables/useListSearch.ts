d04a426f7a15c3e11fadad80f0a2d4f6da8db0e3
less1973 <jama7488@gmail.com>
2023-12-15 22:01:45 +0800

    注释图片

--- src/composables/useListSearch.ts
+++ src/composables/useListSearch.ts
@@ -8,14 +8,14 @@ interface IResetPage {
 }
 
 export function useListSearch(params: ComputedRef, fn: PromiseFn, resetPage: IResetPage) {
-  fn(application.removeEmpty(params.value))
+  // fn(application.removeEmpty(params.value))
 
-  watch(params, (val, oVal) => {
-    if (val.page === oVal.page && oVal.page !== 1) {
-      resetPage()
-      return
-    }
+  // watch(params, (val, oVal) => {
+  //   if (val.page === oVal.page && oVal.page !== 1) {
+  //     resetPage()
+  //     return
+  //   }
 
-    fn(application.removeEmpty(params.value))
-  })
+  //   fn(application.removeEmpty(params.value))
+  // })
 }
--- src/pages/affiliate/commission-rate.vue
+++ src/pages/affiliate/commission-rate.vue
@@ -6,7 +6,7 @@ const { selected: tab, list: tabList } = useSelect(scaleList)
 const {
   data,
   loading,
-} = useRequest(ApiAgencyCommissionScale, { manual: false })
+} = useRequest(ApiAgencyCommissionScale, { manual: true })
 
 const columns: Column[] = [
   {
