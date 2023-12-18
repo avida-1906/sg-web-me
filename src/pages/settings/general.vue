060132ff86c5454b97491aa863e28e069674413d
wade <wade@munuyk.com>
2023-12-18 14:42:15 +0800

    feat:gmail

--- src/pages/settings/general.vue
+++ src/pages/settings/general.vue
@@ -188,6 +188,9 @@ function emailPaste() {
     setEmailDisabledBtnFalse()
   }, 0)
 }
+function goGmail() {
+  window.open(`https://mail.google.com/mail/u/#search/from:@${location.origin}`)
+}
 
 /** 监听邮箱改变 */
 watch(() => email.value, (newValue, oldValue) => {
@@ -328,6 +331,15 @@ onMounted(() => {
           />
         </BaseLabel>
       </div>
+      <template #btm-left>
+        <div v-if="email.includes('@gmail.com')">
+          <BaseButton bg-style="primary" @click="goGmail">
+            <div class="open-gmail">
+              打开 Gmail <BaseIcon name="uni-jump-page" />
+            </div>
+          </BaseButton>
+        </div>
+      </template>
       <template #btm-right>
         <BaseButton
           type="text"
@@ -398,6 +410,15 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
+.open-gmail {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  color: var(--tg-text-white);
+  font-size: 14px;
+  --tg-icon-color: var(--tg-text-white);
+}
 .not-verified-span {
   color: var(--tg-text-white);
 }
