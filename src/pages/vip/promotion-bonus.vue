be9afc90956fd9afe2e88e9f4fa5e591de965f3d
wade <wade@munuyk.com>
2023-12-18 19:51:25 +0800

    feat:ui

--- src/pages/vip/promotion-bonus.vue
+++ src/pages/vip/promotion-bonus.vue
@@ -3,6 +3,7 @@ const params = { ty: 1 }
 
 const { t } = useI18n()
 const { openNotify } = useNotify()
+const { isMobile } = storeToRefs(useWindowStore())
 const { vip, score, vipConfigArray } = useVipInfo()
 
 const { run: runGetPromoBonus, data: promoBonus } = useRequest(ApiMemberVipBonusAvailable)
@@ -51,68 +52,70 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="vip-promotion-bonus">
-    <BaseTable :columns="columns" :data-source="vipConfigArray">
-      <template #level="{ record }">
-        <!-- <div>VIP{{ record.level }}</div> -->
-        <div class="vip-badge">
-          <BaseIcon :name="`vip${record.level}`" />
-        </div>
-      </template>
-      <template #score="{ record }">
-        <div
-          class="score-wrap"
-          :class="{
-            'user-level-vip':
-              +vip + 1 === +record.level,
-            'lower-vip': +record.level <= +vip && bonusArray.length,
-          }"
-        >
-          <span
-            class="text"
-            :style="{ '--progress-width': floor(score / record.score, 1) }"
+  <div class="vip-promotion-bonus" :class="{ 'is-mobile': isMobile }">
+    <div class="tabs">
+      <BaseTable :columns="columns" :data-source="vipConfigArray">
+        <template #level="{ record }">
+          <!-- <div>VIP{{ record.level }}</div> -->
+          <div class="vip-badge">
+            <BaseIcon :name="`vip${record.level}`" />
+          </div>
+        </template>
+        <template #score="{ record }">
+          <div
+            class="score-wrap"
+            :class="{
+              'user-level-vip':
+                +vip + 1 === +record.level,
+              'lower-vip': +record.level <= +vip && bonusArray.length,
+            }"
           >
-            <span v-if="+vip + 1 < +record.level">
-              {{ record.score }}
-            </span>
             <span
-              v-else-if="+vip + 1 === +record.level"
+              class="text"
+              :style="{ '--progress-width': floor(score / record.score, 1) }"
             >
-              {{ score }}/
-              {{ record.score }}
-            </span>
-            <span
-              v-else-if="+vip >= +record.level"
-              class="dark-bar"
-            >
-              <span
-                v-if="bonusArray.length
-                  && bonusArray.filter(b => +b.vip === +record.level
-                    && +b.state === 1).length"
-                class="green-text"
-                @click="() => openReceive(bonusArray.filter(b =>
-                  +b.vip === +record.level && +b.state === 1)[0])"
-              >
-                {{ t('can_receive') }}
+              <span v-if="+vip + 1 < +record.level">
+                {{ record.score }}
               </span>
               <span
-                v-else-if="bonusArray.length
-                  && bonusArray.filter(b => +b.vip === +record.level
-                    && +b.state === 2).length"
+                v-else-if="+vip + 1 === +record.level"
               >
-                {{ t('received') }}</span>
-              <span v-else>{{ t('upgraded') }}</span>
+                {{ score }}/
+                {{ record.score }}
+              </span>
+              <span
+                v-else-if="+vip >= +record.level"
+                class="dark-bar"
+              >
+                <span
+                  v-if="bonusArray.length
+                    && bonusArray.filter(b => +b.vip === +record.level
+                      && +b.state === 1).length"
+                  class="green-text"
+                  @click="() => openReceive(bonusArray.filter(b =>
+                    +b.vip === +record.level && +b.state === 1)[0])"
+                >
+                  {{ t('can_receive') }}
+                </span>
+                <span
+                  v-else-if="bonusArray.length
+                    && bonusArray.filter(b => +b.vip === +record.level
+                      && +b.state === 2).length"
+                >
+                  {{ t('received') }}</span>
+                <span v-else>{{ t('upgraded') }}</span>
               <!-- <span v-else>{{ record.score }}</span> -->
+              </span>
             </span>
-          </span>
-        </div>
-      </template>
-      <template #up_gift="{ record }">
-        <div class="flex-end color-orange">
-          <AppAmount :amount="record.up_gift" currency-type="USDT" />
-        </div>
-      </template>
-    </BaseTable>
+          </div>
+        </template>
+        <template #up_gift="{ record }">
+          <div class="flex-end color-orange">
+            <AppAmount :amount="record.up_gift" currency-type="USDT" />
+          </div>
+        </template>
+      </BaseTable>
+    </div>
     <AppVipRuleDesc />
   </div>
 </template>
@@ -188,6 +191,15 @@ onMounted(() => {
   --tg-table-th-background: var(--tg-secondary-grey);
   --tg-table-even-background: var(--tg-secondary-grey);
   --tg-table-odd-background: var(--tg-primary-main);
+  overflow: visible;
+  &.is-mobile {
+    .tabs {
+    }
+  }
+  .tabs {
+    background: #0F212E;
+    padding: 16px 12px;
+  }
 }
 </style>
 
