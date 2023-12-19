370097852adba6f95731e1f7261eba18b4311bfc
wade <wade@munuyk.com>
2023-12-19 14:21:21 +0800

    feat:ui

--- src/composables/useDialogList.ts
+++ src/composables/useDialogList.ts
@@ -1,15 +1,8 @@
 export const useDialogList = createGlobalState(() => {
   const { bool: closeAllDialog, setBool: setCloseAllDialog } = useBoolean(false)
-  const closeDialog = ref()
-
-  const setDialogClose = (fn: () => void) => {
-    closeDialog.value = fn
-  }
 
   return {
     closeAllDialog,
     setCloseAllDialog,
-    setDialogClose,
-    closeDialog: closeDialog.value,
   }
 })
