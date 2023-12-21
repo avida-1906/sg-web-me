4d5f0a6d0a249ae850c549ddf752fe09af204adf
wade <wade@munuyk.com>
2023-12-21 13:23:58 +0800

    feat:policy

--- src/pages/policies/privacy.vue
+++ src/pages/policies/privacy.vue
@@ -10,17 +10,17 @@ const { openRegisterDialog } = useRegisterDialog()
   <div class="content-block pd-24">
     <h1>Privacy Policy</h1>
     <div class="p">
-      This is the privacy policy of Medium Rare N.V. (owner and operator of Stake.) This Privacy Policy sets out how Stake.com will manage your personal information which we collect as a result of your membership of Stake, your use of our websites located at
-      https://stake.com
-      and any pages or websites under the 'Stake' brand including any mobile phone or tablet applications that are owned and/or operated by us or whenever we otherwise deal with you. We are committed to ensuring that the personal information we collect about you is protected and is used, stored and disclosed in accordance with the Privacy Act and this Privacy Policy.
+      This is the privacy policy of Medium Rare N.V. (owner and operator of Meibo.) This Privacy Policy sets out how Meibo.com will manage your personal information which we collect as a result of your membership of Meibo, your use of our websites located at
+      https://Meibo.com
+      and any pages or websites under the 'Meibo' brand including any mobile phone or tablet applications that are owned and/or operated by us or whenever we otherwise deal with you. We are committed to ensuring that the personal information we collect about you is protected and is used, stored and disclosed in accordance with the Privacy Act and this Privacy Policy.
     </div>
     <h2>1. Website Use</h2>
     <div class="p">
-      In using the website, you agree to be bound by the terms of this Privacy Policy. Stake.com may review the Privacy Policy periodically and reserves the right to change the Privacy Policy at any time at its discretion by posting such changes on the website. Changes will be effective immediately when posted on the website. Your continued use of the website following posting will constitute your acceptance of those changes. This Privacy Policy will not apply to websites which are linked to the website and over which Stake.com has no control.
+      In using the website, you agree to be bound by the terms of this Privacy Policy. Meibo.com may review the Privacy Policy periodically and reserves the right to change the Privacy Policy at any time at its discretion by posting such changes on the website. Changes will be effective immediately when posted on the website. Your continued use of the website following posting will constitute your acceptance of those changes. This Privacy Policy will not apply to websites which are linked to the website and over which Meibo.com has no control.
     </div>
     <h2>2. Personal Information</h2>
     <div class="p">
-      The kinds of Personal Information that Stake.com may collect from you is:
+      The kinds of Personal Information that Meibo.com may collect from you is:
     </div>
     <ul>
       <li>
@@ -96,7 +96,7 @@ const { openRegisterDialog } = useRegisterDialog()
     </ul>
     <h2>3. Collection Purposes</h2>
     <div class="p">
-      Stake.com will collect your Personal Information only by lawful and fair means and not in an intrusive way to operate its business as a licensed online wagering bookmaker for the following purposes:
+      Meibo.com will collect your Personal Information only by lawful and fair means and not in an intrusive way to operate its business as a licensed online wagering bookmaker for the following purposes:
     </div>
     <ul>
       <li>
@@ -106,12 +106,12 @@ const { openRegisterDialog } = useRegisterDialog()
       </li>
       <li>
         <div class="p">
-          If you wish to subscribe to Stake.com's marketing communications.
+          If you wish to subscribe to Meibo.com's marketing communications.
         </div>
       </li>
       <li>
         <div class="p">
-          To process your application to become a Member of Stake.com or any managed subdomains.
+          To process your application to become a Member of Meibo.com or any managed subdomains.
         </div>
       </li>
       <li>
@@ -141,7 +141,7 @@ const { openRegisterDialog } = useRegisterDialog()
       </li>
       <li>
         <div class="p">
-          To create aggregate data about Members through demographic profiling, statistical analysis of the database to provide to potential and existing stakeholders, and to allow for more efficient operation of Stake.com’s business.
+          To create aggregate data about Members through demographic profiling, statistical analysis of the database to provide to potential and existing Meiboholders, and to allow for more efficient operation of Meibo.com’s business.
         </div>
       </li>
       <li>
@@ -156,12 +156,12 @@ const { openRegisterDialog } = useRegisterDialog()
       </li>
       <li>
         <div class="p">
-          To comply with Stake.com’s contractual, legal, and statutory obligations.
+          To comply with Meibo.com’s contractual, legal, and statutory obligations.
         </div>
       </li>
       <li>
         <div class="p">
-          For taking appropriate action if Stake.com has reason to suspect that unlawful activity or misconduct of a serious nature has been, is being or may be engaged in that relates to our functions and activities.
+          For taking appropriate action if Meibo.com has reason to suspect that unlawful activity or misconduct of a serious nature has been, is being or may be engaged in that relates to our functions and activities.
         </div>
       </li>
       <li>
@@ -176,31 +176,31 @@ const { openRegisterDialog } = useRegisterDialog()
       </li>
     </ul>
     <div class="p">
-      If you do not provide Stake.com with the Personal Information we request, we may be unable to process your application to become a Member, provide you with our services or respond to your enquiry or complaint.
+      If you do not provide Meibo.com with the Personal Information we request, we may be unable to process your application to become a Member, provide you with our services or respond to your enquiry or complaint.
     </div>
     <div class="p">
-      By using Stake.com, becoming a Member or otherwise choosing to provide Stake.com with Personal Information you consent to Stake.com collecting, using, processing and potentially disclosing your Personal Information for the above purposes and on the bases described in this Privacy Policy.
+      By using Meibo.com, becoming a Member or otherwise choosing to provide Meibo.com with Personal Information you consent to Meibo.com collecting, using, processing and potentially disclosing your Personal Information for the above purposes and on the bases described in this Privacy Policy.
     </div>
     <h2>4. Direct Marketing and Opting Out</h2>
     <div class="p">
-      From time to time we may use your Personal Information to inform you about our products or services or about promotional activities which Stake.com believes may be of interest or of benefit to you. We may do this via email, SMS, telephone or mail. If you no longer wish to receive marketing or promotional material from Stake.com at all or in any particular form, you may contact us at any time by email to support@stake.com with your request with which we will comply as soon as is practical.
+      From time to time we may use your Personal Information to inform you about our products or services or about promotional activities which Meibo.com believes may be of interest or of benefit to you. We may do this via email, SMS, telephone or mail. If you no longer wish to receive marketing or promotional material from Meibo.com at all or in any particular form, you may contact us at any time by email to support@Meibo.com with your request with which we will comply as soon as is practical.
     </div>
     <div class="p">
-      From time to time we may contact you in relation to the management and administration of your Stake.com account. These communications can be via any of the modes of contact recorded when registering as a Member. Such communication does not affect your opt-in or opt-out status for direct marketing communications.
+      From time to time we may contact you in relation to the management and administration of your Meibo.com account. These communications can be via any of the modes of contact recorded when registering as a Member. Such communication does not affect your opt-in or opt-out status for direct marketing communications.
     </div>
     <h2>5. Management of your Personal Information</h2>
     <div class="p">
-      Stake.com will take all reasonable steps to ensure that the Personal Information which it collects, uses or discloses is correct and is stored in a secure environment which is accessed only by authorised persons. We store the personal information we receive as described in this Privacy Policy for as long as you use our Services or as necessary to fulfill the purpose(s) for which it was collected, provide our Services, resolve disputes, establish legal defenses, conduct audits, pursue legitimate business purposes, enforce our agreements, and comply with applicable laws.
+      Meibo.com will take all reasonable steps to ensure that the Personal Information which it collects, uses or discloses is correct and is stored in a secure environment which is accessed only by authorised persons. We store the personal information we receive as described in this Privacy Policy for as long as you use our Services or as necessary to fulfill the purpose(s) for which it was collected, provide our Services, resolve disputes, establish legal defenses, conduct audits, pursue legitimate business purposes, enforce our agreements, and comply with applicable laws.
     </div>
     <div class="p">
-      Stake.com will destroy or permanently de-identify the Personal Information we hold when it is no longer required for any purpose permitted under the APPs including our legal or operational obligations.
+      Meibo.com will destroy or permanently de-identify the Personal Information we hold when it is no longer required for any purpose permitted under the APPs including our legal or operational obligations.
     </div>
     <h2>6. Security of Personal Information</h2>
     <div class="p">
-      You acknowledge that no data transmission over the Internet is totally secure. Accordingly, Stake.com does not guarantee or warrant the security of any information which you transmit to it. Any information which you transmit to Stake.com is transmitted at your own risk. However, once Stake.com receives your transmission, Stake.com will take reasonable steps to protect your Personal Information from misuse, loss and unauthorized access, modification and disclosure including by using password protected systems and databases and Secure Socket Layer (SSL) technology. By using our Services or providing personal information to us, you agree that we may communicate with you electronically regarding security, privacy, and administrative issues relating to your use of the Services.
+      You acknowledge that no data transmission over the Internet is totally secure. Accordingly, Meibo.com does not guarantee or warrant the security of any information which you transmit to it. Any information which you transmit to Meibo.com is transmitted at your own risk. However, once Meibo.com receives your transmission, Meibo.com will take reasonable steps to protect your Personal Information from misuse, loss and unauthorized access, modification and disclosure including by using password protected systems and databases and Secure Socket Layer (SSL) technology. By using our Services or providing personal information to us, you agree that we may communicate with you electronically regarding security, privacy, and administrative issues relating to your use of the Services.
     </div>
     <div class="p">
-      Stake.com’s employees, agents and contractors are required to maintain the confidentiality of Members' Personal Information and trading behavior. Information posted on bulletin boards or communicated within a social media environment (for example, Facebook, Twitter, Chat Rooms) becomes public information. Stake.com cannot guarantee the security of this type of disclosed information.
+      Meibo.com’s employees, agents and contractors are required to maintain the confidentiality of Members' Personal Information and trading behavior. Information posted on bulletin boards or communicated within a social media environment (for example, Facebook, Twitter, Chat Rooms) becomes public information. Meibo.com cannot guarantee the security of this type of disclosed information.
     </div>
     <div class="p">
       We take seriously the responsibility to exclude children from access to our services. We will not accept their information for the purposes of opening a wagering account. It is however, ultimately the responsibility of parents or guardians to monitor their children’s Internet activities including where appropriate by using Internet screening software.
@@ -210,6 +210,56 @@ const { openRegisterDialog } = useRegisterDialog()
     </div>
     <div class="p">
       Ultimately, you are solely responsible for maintaining the secrecy of your username, password and any account information. Please be careful whenever using the Internet and our website.
+    </div>
+    <h2>7. Access to Personal Information</h2>
+    <div class="p">
+      You may access the Personal Information collected by Meibo.com by following the “Settings” link on the website or by contacting us on support@Meibo.com. We will give you access to your Personal Information in the manner requested if that is possible and within a reasonable period. If we refuse your request or cannot give you access in the manner you have requested, we will do what we can to meet your requirements by other means. We may not be required to give you access to your Personal Information in certain circumstances which are set out in the APPs including where it may have an unreasonable impact on another individual's privacy. If we refuse access for such reasons, we will advise you in writing of the refusal and our reasons and the complaint mechanisms available to you.
+    </div>
+    <h2>8. Delete Personal Data</h2>
+    <div class="p">
+      You can request to have your personal data deleted if Meibo.com no longer have a legal reason to continue to process or store it. Please note that this right is not guaranteed - in the sense that Meibo.com do not have the ability to comply with your request if it is subject to a legal obligation to store your data. You can request the deletion of your personal data by sending an email to support@Meibo.com.
+    </div>
+    <h2>9. Contact Details</h2>
+    <div class="p">
+      If you have any queries, requests for access or correction or complaints relating to the handling of your personal information, please contact us by email at support@Meibo.com or by the site customer support chat function.
+    </div>
+    <h2>10. International Data Transfers</h2>
+    <div class="p">
+      All information processed by us may be transferred, processed, and stored anywhere in the world, including but not limited to other countries, which may have data protection laws that are different from the laws where you live. We endeavor to safeguard your information consistent with the requirements of applicable laws.
+    </div>
+    <h2>11. Legal Basis for Processing</h2>
+    <div class="p">
+      For purposes of the EU General Data Protection Regulation, Meibo.com’s processing of your personal information is supported by the following lawful bases:
+    </div>
+    <ul>
+      <li>
+        <div class="p">
+          Performance of a Contract: Meibo.com needs to process your personal information to perform our contract with you or our Customer.
+        </div>
+      </li>
+      <li>
+        <div class="p">
+          Legitimate Interest: Meibo.com processes your personal information to further our legitimate interests but only where our interests are not overridden by your interests or fundamental rights and freedoms.
+        </div>
+      </li>
+      <li>
+        <div class="p">
+          Consent: In some cases, Meibo.com also relies on your consent to process your personal information.
+        </div>
+      </li>
+      <li>
+        <div class="p">
+          Compliance with Legal Obligations: Meibo.com may process your personal information to comply with our legal obligations.
+        </div>
+      </li>
+    </ul>
+    <h2>12. Supervisor Authority</h2>
+    <div class="p">
+      If you are located in the European Economic Area, you have the right to lodge a complaint with a supervisory authority if you believe our processing of your personal information violates applicable law.
+    </div>
+    <h2>13. Updates to this Privacy Policy</h2>
+    <div class="p">
+      As noted, Meibo.com may review, change and update this Privacy Policy from time to time reflect our current practices and obligations. We will publish our current Privacy Policy on our website at https://Meibo.com and the changes will take effect at the time of publishing. You should review this privacy policy regularly and remain familiar with its terms.
     </div>
     <div v-if="!isLogin" class="p buttons">
       <BaseButton type="text" size="none" @click="openLoginDialog()">
