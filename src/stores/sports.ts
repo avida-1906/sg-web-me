86a080c8aa8de41ec19f1aa0f9e98e7176f66823
Roger <Roger@123.com>
2023-12-22 20:00:17 +0800

    fix🐛: 默认让分盘

--- src/stores/sports.ts
+++ src/stores/sports.ts
@@ -312,7 +312,7 @@ export const useSportsStore = defineStore('sports', () => {
   /** 获取球种对应的盘口类型下拉选单 */
   function getSportsBetTypeListBySi(si: number) {
     return sportsBetTypeList.value.find(a => a.si === si)?.btl
-    ?? [{ value: 3, label: t('sports_winner_market') }]
+    ?? [{ value: 1, label: t('sports_handicap_market') }]
   }
 
   /** 获取球种对应icon */
