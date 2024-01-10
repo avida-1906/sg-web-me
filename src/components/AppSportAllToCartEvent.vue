95fd7f4ed239d6ef7d7d00072d0a74f14cba52e3
less1973 <jama7488@gmail.com>
2024-01-10 18:25:14 +0800

    添加体育通知组件

---
+++ src/components/AppSportAllToCartEvent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const emits = defineEmits(['receive'])
+
+// // 体育所有列表，详情向 购物车 推送消息
+function handle() {
+  emits('receive')
+}
+function send() {
+  sportsToCartBus.emit()
+}
+
+defineExpose({ send })
+
+onMounted(() => {
+  cartToSportsBus.on(handle)
+})
+
+onUnmounted(() => {
+  cartToSportsBus.off(handle)
+})
+</script>
+
+<template>
+  <div class="h-0 overflow-hidden text-white" />
+</template>
