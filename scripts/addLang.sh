dc0062bd9c5a0b75846fb5d24a3d94c0a8ec6b30
less1973 <jama7488@gmail.com>
2024-01-17 12:21:49 +0800

    更新语言文件

--- scripts/addLang.sh
+++ scripts/addLang.sh
@@ -16,9 +16,15 @@ fi
 # 遍历 locales 目录下的所有 .yml 文件
 for file in ${LOCALES_DIR}/*.yml; do
     if [ -f "$file" ]; then
-        # 在文件末尾添加一个新行，然后追加 lang.yml 的内容
-        echo >> "${file}" # 添加一个新行
-        cat "${LANG_FILE}" >> "${file}"
+        # 检查文件的最后一行是否为空
+        if [ -z "$(tail -n 1 "${file}")" ]; then
+            # 最后一行为空，直接追加 lang.yml 的内容
+            cat "${LANG_FILE}" >> "${file}"
+        else
+            # 最后一行不为空，先添加一个新行再追加内容
+            echo >> "${file}"
+            cat "${LANG_FILE}" >> "${file}"
+        fi
         echo "Added content to ${file}"
     fi
 done
