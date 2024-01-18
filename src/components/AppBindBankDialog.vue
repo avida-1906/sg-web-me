9e0b400aaf7d14fe0e0c6f3d33222dda18111390
less1973 <jama7488@gmail.com>
2024-01-18 18:05:32 +0800

    添加银行卡参数调整

--- src/components/AppBindBankDialog.vue
+++ src/components/AppBindBankDialog.vue
@@ -2,34 +2,20 @@
 import { useForm } from 'vee-validate'
 import * as yup from 'yup'
 
+interface IBank {
+  id: string
+  name: string
+  pid: string
+  sortlevel: string
+}
+
 const { t } = useI18n()
+const currencyId = ref('701')
+const auth_type = ref<1 | 2>(1)
 // 是否设置为默认地址
 const { bool: isDefaultAddress, setBool: setDefaultAddress } = useBoolean(false)
-const countryOptions = ref<ISelectOption[]>([
-  { label: '中国', value: '1137' },
-  { label: '美国', value: '1138' },
-])
-const bank_nameOptions = ref<ISelectOption[]>([
-  { label: '中国银行', value: '中国银行' },
-  { label: '中国农业银行', value: '中国农业银行' },
-])
 
-/**
- * {
-    "currency_id": "701",
-    "open_name": "张三1", //开户人姓名，如：李四
-    "bank_name": "农业银行", //银行名称，如：农业银行
-    "bank_account": "66232234112216261231", //银行账号
-    "bank_area_cpf": "1111", //开户地区，如：广东省深圳市龙华
-    "is_default": 1, //是否默认：1：是，2否
-    "pay_password": "123456",
-    "country": "1137", //国家
-    "city": "shanghai", //城市
-    "address": "abc test" //详细地址
-  }
- */
-
-// 1.3验证规则，核心
+// #region 表单验证
 const validationSchema = yup.object({
   xing: yup.string().trim().required(t('enter_your_content')),
   ming: yup.string().trim().required(t('enter_your_content')),
@@ -40,7 +26,9 @@ const validationSchema = yup.object({
   bank_account: yup.number().required(t('enter_your_content')).typeError('银行卡号必须是数字'),
   pay_password: yup.string().trim().required(t('enter_your_content')),
 })
+// #endregion
 
+// #region 表单数据
 const { values, errors, handleSubmit, resetForm } = useForm({ validationSchema })
 const { value: xing, errorMessage: xingError } = useField<string>('xing')
 const { value: ming, errorMessage: mingError } = useField<string>('ming')
@@ -50,7 +38,9 @@ const { value: address, errorMessage: addressError } = useField<string>('address
 const { value: bank_name, errorMessage: bank_nameError } = useField<string>('bank_name')
 const { value: bank_account, errorMessage: bank_accountError } = useField<number>('bank_account')
 const { value: pay_password, errorMessage: pay_passwordError } = useField<string>('pay_password')
+// #endregion
 
+// #region 请求api
 const { openNotify } = useNotify()
 const {
   run: runBankcardInsert,
@@ -64,20 +54,74 @@ const {
     })
   },
 })
+// #endregion
 
+// #region 银行数据
+const {
+  data: bankList,
+  run: runBankTreeList,
+} = useApiMemberTreeList('019001')
+
+const bank_nameOptions = computed(() => {
+  if (!bankList.value)
+    return []
+  return bankList.value.map((item: IBank) => {
+    const temp = {
+      label: item.name,
+      icon: 'fiat-bank',
+      value: item.name,
+    }
+    return temp
+  })
+})
+// #endregion
+
+// #region 国家数据
+const {
+  data: countryList,
+  run: runCountryList,
+} = useApiMemberTreeList('006')
+
+const countryOptions = computed(() => {
+  if (!countryList.value)
+    return []
+  return countryList.value.map((item: IBank) => {
+    const temp = {
+      label: item.name,
+      value: item.id,
+    }
+    return temp
+  })
+})
+// #endregion
+
+// #region 提交表单
 // 3、表单行为===============================================================================
 // 提交表单：验证成功的处理和验证失败的处理
 // 第一个参数：验证成功回调，入参为表单数据
 // 第二个参数：验证失败回调，入参为表单数据、错误信息、验证结果
-// 验证成功后，调用API，resetForm()，重置表单
+// 验证成功后，调用 resetForm()，重置表单
 const onSubmit = handleSubmit((values) => {
   console.log('发送表单数据', values)
+  runBankcardInsert({
+    currency_id: currencyId.value,
+    open_name: values.xing + values.ming,
+    bank_name: values.bank_name,
+    bank_account: values.bank_account,
+    is_default: isDefaultAddress.value ? 1 : 2,
+    pay_password: values.pay_password,
+    country: values.country,
+    city: values.city,
+    address: values.address,
+    auth_type: auth_type.value,
+  })
   // resetForm() // 重置表单
 }, (values, errors, results) => {
   console.log('表单数据', values)
   console.log('错误信息', errors)
   console.log('验证结果', results)
 })
+// #endregion
 </script>
 
 <template>
@@ -133,6 +177,7 @@ const onSubmit = handleSubmit((values) => {
           {{ $t('check_account_pls') }}
         </div>
         <BaseButton
+          :loading="bankcardInsertLoading"
           bg-style="secondary"
           size="sm"
           class="min-w-90"
@@ -142,12 +187,5 @@ const onSubmit = handleSubmit((values) => {
         </BaseButton>
       </div>
     </form>
-
-    <div class="content200">
-      <h3>表单所有字段值</h3>
-      <h6>{{ values }}</h6>
-      <h3>表单所有错误信息</h3>
-      <h6>{{ errors }}</h6>
-    </div>
   </div>
 </template>
