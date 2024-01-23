deac9618a2475a15c00757ed066cb154641d2a39
Roger <Roger@123.com>
2024-01-23 15:38:11 +0800

    fix🐛: STAK-3185 【PC/H5】钱包兑换货币输入支付金额时汇率显示错误

--- src/components/AppMoneyExchange.vue
+++ src/components/AppMoneyExchange.vue
@@ -66,7 +66,7 @@ const currencyGetOptions = computed(() => {
 })
 const rate = computed(() => {
   const r = getRate(currencyTypePay.value, currencyTypeGet.value, 9)?.targetNum
-  return r ? +r : 1
+  return scientificToString(r ? +r : 1)
 })
 const currencyMaxBalance = computed(() => userInfo.value?.balance[currencyTypePay.value] ?? 0)
 
